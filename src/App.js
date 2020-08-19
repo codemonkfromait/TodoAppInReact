16773b60d41c708b96a13edf50200e879845157c
Pradeep <pradeeprocks1999@gmail.com>
2020-08-19 11:40:48 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ThemeContext from "./Context/ThemeContext";
+import Header from "./Components/Header";
 
-function App() {
+export default function App() {
+  const themeinitial = useState("light");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={themeinitial}>
+      <Header />
+    </ThemeContext.Provider>
   );
 }
-
-export default App;
---
+++ src/Colors.js
@@ -0,0 +1,16 @@
+const Apptheme = {
+  light: {
+    textColor: "#000",
+    backgroundColor: "#99AAAB",
+    border: "4px solid #000",
+    fontSize: "20px",
+  },
+  dark: {
+    textColor: "#E71C23",
+    backgroundColor: "#2C3335",
+    border: "4px solid #000",
+    fontSize: "20px",
+  },
+};
+
+export default Apptheme;
